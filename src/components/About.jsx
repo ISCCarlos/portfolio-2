91e95c4bd06b2c58107a977b279c3e23b72d48da
ISCCarlos <carlos.chavez.arias@gmail.com>
2022-12-07 19:50:53 -0600

    N/A: Added about styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import { Divider, Typography } from '@mui/material'
+import { Avatar, Card, Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
 import { useTranslation } from "react-i18next";
 
@@ -10,13 +10,37 @@ const About = () => {
       <Divider component="div" role="presentation">
         <Typography variant="h3">{translations("about.title")}</Typography>
       </Divider>
-      <Typography variant="h6" mt={5}>
-        {translations("about.content", { experience: 6 })}
-      </Typography>
-      <Typography variant="h6">
-        {translations("about.content1")}
-      </Typography>
-    </div>
+      <Card sx={{ height: '85vh' }}>
+        <Grid container spacing={2}>
+          <Grid item sm={6} xs={12}>
+            <Avatar
+              alt="chavez-dev"
+              src='../../assets/me.jpg'
+              sx={{
+                width: { xs: '100%', sm: '85%' },
+                height: { xs: '100%', sm: '85%' },
+                marginTop: { xs: 'none', sm: 5 },
+                marginLeft: { xs: 'none', sm: 10 }
+              }}
+            />
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <Typography variant="h6" mt={{ sm: 10, xs: 1 }} ml={{ sm: 10, xs: 'none' }} width={{ sm: '80%', xs: '100%' }}>
+              {translations("about.contentParagraph0", { experience: 6 })}
+            </Typography>
+            <Typography variant="h6" mt={{ sm: 10, xs: 1 }} ml={{ sm: 10, xs: 'none' }} width={{ sm: '80%', xs: '100%' }}>
+              {translations("about.contentParagraph1")}
+            </Typography>
+            <Typography variant="h6" mt={{ sm: 10, xs: 1 }} ml={{ sm: 10, xs: 'none' }} width={{ sm: '80%', xs: '100%' }}>
+              {translations("about.contentParagraph2")}
+            </Typography>
+            <Typography variant="h6" mt={{ sm: 10, xs: 1 }} ml={{ sm: 10, xs: 'none' }} width={{ sm: '80%', xs: '100%' }}>
+              {translations("about.contentParagraph3")}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Card>
+    </div >
   )
 }
 
